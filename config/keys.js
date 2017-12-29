3701ae44b924d62b1649fd8839bfd456f5ba9931
Russell Wright <russell.bwright@yahoo.com>
2017-12-28 19:30:55 -0600

    set up login and cookies

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if(process.env.NODE_ENV === 'production') {
+    //we are in prod - return prod keys
+    module.exports = require('./prod')
+} else {
+    //we are in dev - return dev keys
+    module.exports = require('./dev')
+}
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys')
+const mongoose = require('mongoose')
 
+const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+     .then((user) => {
+        done(null, user);
+     })
+})
 
 passport.use(
     new GoogleStrategy({
@@ -9,9 +22,19 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refesh', refreshToken);
-        console.log('profie', profile);
+        User.findOne({ googleID: profile.id })
+        .then((existingUser) => {
+            if(existingUser) {
+                //we already have a record with the given id
+                done(null, existingUser);
+            } else {
+                //need to make a new record
+                new User ({ googleID: profile.id }).save()
+                .then(user => done(null, user)); 
+            }
+        })
+
+            
     }
   )
 );
