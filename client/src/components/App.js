9da80b59e8f3f67ec5fac2ce35c2ab9faa4c1052
Russell Wright <russell.bwright@yahoo.com>
2018-01-05 16:49:43 -0600

    added materialize, and started header function

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-const Header = () => <h2>Header </h2>
+import Header from "./Header"
+
+
 const Dashboard = () => <h2>Dashboard </h2>
 const SurveryNew = () => <h2>SurveryNew </h2>
 const Landing = () => <h2>Landing</h2>
@@ -11,7 +13,10 @@ const App = () => {
         <div>
            <BrowserRouter>
               <div>
-                <Route path="/" component={Landing} />
+                <Header />
+                <Route exact={true} path="/" component={Landing} />
+                <Route exact path="/surveys" component={Dashboard} />
+                <Route path="/surveys/new" component={SurveryNew} />
               </div>    
            </BrowserRouter>
         </div>
