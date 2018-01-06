c95e9101313c806c1bffd3ca241e998de635add7
Russell B W <russell.bwright@yahoo.com>
2018-01-06 00:32:43 -0600

    more redux

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
-import { FETCH_USER } from './types';
+import axios from "axios";
+import { FETCH_USER } from "./types";
 
-const fetchUser = () => {
-    axios.get('/api/current_user').then((res) => {
-        console.log(res)
-    })
-}
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user")
+  
+  dispatch({ type: FETCH_USER, payload: res})
+        
+};
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,33 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from 'react-redux';
+import * as actions from '../actions'
 
-import Header from "./Header"
+import Header from "./Header";
 
+const Dashboard = () => <h2>Dashboard </h2>;
+const SurveryNew = () => <h2>SurveryNew </h2>;
+const Landing = () => <h2>Landing</h2>;
 
-const Dashboard = () => <h2>Dashboard </h2>
-const SurveryNew = () => <h2>SurveryNew </h2>
-const Landing = () => <h2>Landing</h2>
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
 
-const App = () => {
+  render() {
     return (
-        <div className="container">
-           <BrowserRouter>
-              <div>
-                <Header />
-                <Route exact={true} path="/" component={Landing} />
-                <Route exact path="/surveys" component={Dashboard} />
-                <Route path="/surveys/new" component={SurveryNew} />
-              </div>    
-           </BrowserRouter>
-        </div>
-    );        
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact={true} path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveryNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
-};
-
-export default App;
+export default connect(null, actions)(App);
