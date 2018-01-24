1c1bcc79e02791a38885fdae29987f53d2339f30
Russell B W <russell.bwright@yahoo.com>
2018-01-24 16:39:42 -0600

    added stripe to backend server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieSession = require('cookie-session')
-const passport = require('passport')
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser')
+
 require('./models/User')
 require('./services/passport')
 
@@ -11,6 +13,7 @@ mongoose.connect(keys.mongoURI)
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -21,6 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 
 
