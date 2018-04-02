17b73ec6346b7c9553b0dd560e66efb4d7b08eef
Russell B W <russell.bwright@yahoo.com>
2018-04-02 16:12:53 -0500

    added schemas to mongo db

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
-require('./models/User')
-require('./services/passport')
+require('./models/User');
+require('./models/Survey');
+require('./services/passport');
 
 const keys = require('./config/keys')
 
